c1b56ed487e3c2f0b15dee0e93630e8ef6bf7c6c
alexandreev1 <alexandreev0210@gmail.com>
2019-11-30 19:25:50 +0300

    Сортировка пузырьком, новое представление данных

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-let arr = [];
+let elArr = [];
 let optionValue = "shell";
 let rangeValue = 10;
 
@@ -10,34 +10,33 @@ let display = document.getElementById("display");
 let randomButton = document.getElementById("randomButton");
 let contentPlace = document.getElementById("contentPlace");
 
-input.addEventListener("change", (e) => {
-    arr = e.target.value.split(/[, ]/g);
-    for(let i = 0; i < arr.length; i++) {
-        arr[i] = parseInt(arr[i]);
-    }
+input.addEventListener("change", e => {
+    elArr = [];
+    e.target.value.split(/[, ]/g).forEach(el => elArr.push({el: null, value: el.toString()}));
+    displayCurrentArray(elArr);
+    makeABubble(elArr);
 });
 
-select.addEventListener("change", (e) => {
+select.addEventListener("change", e => {
     optionValue = e.target.value;
 });
 
-range.addEventListener("change", (e) => {
-    rangeValue = parseInt(e.target.value);
+range.addEventListener("change", e => {
+    rangeValue = parseInt(e.target.value, 10);
 });
 
 randomButton.addEventListener("click", () => {
-    let randArr = getRandom(1, 99, 8);
-    console.log(randArr);
-    displayCurrentArray(randArr);
-    for(let i = 0; i < randArr.length; i++) {
-        if (!document.getElementById(i.toString())) {
-            let bubble = document.createElement("div");
-            bubble.className = "bubble";
-            bubble.innerHTML = randArr[i].toString();
-            bubble.id = i.toString();
-            contentPlace.appendChild(bubble);
-        }
-    }
+    elArr = [];
+    elArr = getRandom(1, 99, 8);
+    console.log(elArr);
+    displayCurrentArray(elArr);
+    makeABubble(elArr);
+});
+
+button.addEventListener("click", () => {
+    elArr = bubbleSort(elArr);
+    displayCurrentArray(elArr);
+    input.value = "";
 });
 
 //описывем функцию-рандомайзер для быстрого получения массива значений
@@ -45,17 +44,60 @@ randomButton.addEventListener("click", () => {
 function getRandom(from, to, size) {
     let randArr = [];
     for (let i = 0; i < size; i++) {
-        randArr.push(Math.floor(+from + Math.random() * (+to + 1 - +from)));
+        randArr.push({el: null, value: Math.floor(+from + Math.random() * (+to + 1 - +from))});
     }
     return randArr;
 }
 
 //для отображения того, что мы вообще ввели в начале, в т.ч. рандомные значения
 function displayCurrentArray(arr) {
-    if(!document.getElementById("theOne")) {
-        let currentArray = document.createElement("h1");
-        currentArray.id = "theOne";
-        currentArray.innerHTML = arr.join(" ");
-        display.appendChild(currentArray);
+    let shown = document.getElementById("theOne");
+
+    if (!shown) {
+        let shown = document.createElement("h1");
+        shown.id = "theOne";
+        shown.innerHTML = arr.map(el => el.value).join(" ");
+        display.appendChild(shown);
+        return;
     }
+    shown.innerHTML = arr.map(el => el.value).join(" ");
+}
+
+function makeABubble(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let bubble = document.getElementById(`bubble-${i.toString()}`);
+        if (!bubble) {
+            let bubble = document.createElement("div");
+            bubble.className = "bubble";
+            bubble.style.top = `${56 * i}px`;
+            bubble.innerHTML = arr[i].value.toString();
+            bubble.id = `bubble-${i.toString()}`;
+            contentPlace.appendChild(bubble);
+            elArr[i] = ({el: bubble, value: arr[i].value});
+            continue;
+        }
+        bubble.className = "bubble";
+        bubble.style.top = `${56 * i}px`;
+        bubble.innerHTML = arr[i].value.toString();
+        elArr[i] = ({el: bubble, value: arr[i].value});
+    }
+    console.log(elArr);
+}
+
+//сортировка "пузырьком"
+function bubbleSort(arr) {
+    for (let i = 0, endI = arr.length - 1; i < endI; i++) {
+        let wasSwap = false;
+        for (let j = 0, endJ = endI - i; j < endJ; j++) {
+            if (arr[j].value > arr[j + 1].value) {
+                let swap = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
+                wasSwap = true;
+            }
+        }
+        if (!wasSwap) break;
+    }
+    console.log(arr);
+    return arr;
 }
